b717f7e9f0d7da49fb0ee1dbcc33165c709f5e1b
Peter Todd <peter@petertodd.com>
2013-05-14 19:04:58 +0100

    Version 1.1
    Supports ISO A4 and USA Letter.
    Resizes header and body images to fit with CGRect bounds without
    distortion.
    Displays formatted labels.
    Email and Print functionality.

---
+++ BGSRenderModel2.h
@@ -0,0 +1,13 @@
+//
+//  BGSRenderModel2.h
+//  BGS PDF templates
+//
+//  Created by Peter Todd on 14/05/2013.
+//  Copyright (c) 2013 BrightGreenStar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BGSRenderModel2 : NSObject
+
+@end
---
+++ BGSRenderModel2.m
@@ -0,0 +1,13 @@
+//
+//  BGSRenderModel2.m
+//  BGS PDF templates
+//
+//  Created by Peter Todd on 14/05/2013.
+//  Copyright (c) 2013 BrightGreenStar. All rights reserved.
+//
+
+#import "BGSRenderModel2.h"
+
+@implementation BGSRenderModel2
+
+@end
